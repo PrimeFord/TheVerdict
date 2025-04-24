98681c4f0b41e8d51b6f229f5d29a165d896d24f
Prime <wolabash@gmail.com>
2025-04-24 15:09:45 +0100

    Feat: The Verdict Codebase (First Commit)

---
+++ src/main/java/com/theverdict/theverdict/TheverdictApplication.java
@@ -0,0 +1,13 @@
+package com.theverdict.theverdict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheverdictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheverdictApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/theverdict/theverdict/TheverdictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theverdict.theverdict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheverdictApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
